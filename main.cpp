2e4356dde271e08e6304e1cc6fb02c9c014d7f34
Maxim Lazarev <maxim.lazarev@compvisionsys.com>
2022-11-09 00:14:48 +0300

    vers. 1.0

--- main.cpp
+++ main.cpp
@@ -2,144 +2,150 @@
 #include <functional>
 #include <iostream>
 #include <map>
+#include <set>
 #include <vector>
 
-struct Node
-{
+struct Node {
   int nodeId;
   std::vector<int> nodeDependences;
 };
 
-namespace
-{
-  typedef std::map<int, int> ColorDictionary;
-  typedef std::pair<int, const int> NodeColor;
+namespace {
+typedef std::map<int, int> ColorDictionary;
+typedef std::pair<int, const int> NodeColor;
 
-  enum TarjanColor
-  {
-    WHITE = 0,
-    BLACK = 1,
-    GRAY = 2
-  };
+enum TarjanColor { WHITE = 0, BLACK = 1, GRAY = 2 };
 
-  Node &nodeById_(std::vector<Node> &graph, int nodeId)
-  {
-    for (auto &node : graph)
-    {
-      if (node.nodeId == nodeId)
-      {
-        return node;
-      }
+Node &nodeById_(std::vector<Node> &graph, int nodeId) {
+  for (auto &node : graph) {
+    if (node.nodeId == nodeId) {
+      return node;
+    }
+  }
+}
+
+std::vector<Node> inverse_(const std::vector<Node> &graph) {
+  std::vector<Node> inversedGraph = graph;
+  for (auto &node : inversedGraph) {
+    node.nodeDependences.clear();
+  }
+
+  for (auto &node : graph) {
+    for (auto &dependenceId : node.nodeDependences) {
+      Node &insertNode = nodeById_(inversedGraph, dependenceId);
+      insertNode.nodeDependences.push_back(node.nodeId);
     }
   }
 
-  std::vector<Node> inverse_(const std::vector<Node> &graph)
-  {
-    std::vector<Node> inversedGraph = graph;
-    for (auto &node : inversedGraph)
-    {
-      node.nodeDependences.clear();
-    }
+  return inversedGraph;
+}
 
-    for (auto &node : graph)
-    {
-      for (auto &dependenceId : node.nodeDependences)
-      {
-        Node &insertNode = nodeById_(inversedGraph, dependenceId);
-        insertNode.nodeDependences.push_back(node.nodeId);
-      }
+bool dependencesAreBlack_(Node &node, ColorDictionary &graphColor) {
+  bool statement = true;
+  for (auto &index : node.nodeDependences) {
+    if (graphColor[index] != BLACK) {
+      statement = false;
     }
-
-    return inversedGraph;
   }
 
-  bool dependencesAreBlack_(Node &node, ColorDictionary &graphColor)
-  {
-    bool statement = true;
-    for (auto &index : node.nodeDependences)
-    {
-      statement = (graphColor[index] != BLACK) ? false : true;
-    }
+  return statement;
+}
 
-    return statement;
+bool nodeIsEdge_(Node &node, ColorDictionary &graphColor) {
+  return (node.nodeDependences.empty() ||
+          dependencesAreBlack_(node, graphColor));
+}
+
+bool nodesAreBlack_(std::vector<Node> &nodes, ColorDictionary &graphColor) {
+  bool statement = true;
+  for (auto &node : nodes) {
+    if (not graphColor[node.nodeId] == BLACK) {
+      statement = false;
+    }
+  }
+  return statement;
+}
+
+void deepSearchEdges_(ColorDictionary &graphColor, std::vector<Node> &newGraph,
+                      std::vector<Node> &oldGraph, Node &currentNode) {
+  if (graphColor[currentNode.nodeId] == GRAY) {
+    throw "cicle found";
   }
 
-  void deepSearchSorting_(ColorDictionary &graphColor, std::vector<Node> &newGraph,
-                          std::vector<Node> &oldGraph, Node &currentNode)
-  {
-    if (graphColor[currentNode.nodeId] == GRAY)
-    {
-      throw "cicle found";
-    }
+  if (graphColor[currentNode.nodeId] == BLACK) {
+    return;
+  }
 
-    if (graphColor[currentNode.nodeId] == BLACK)
-    {
-      return;
-    }
-
-    if (currentNode.nodeDependences.empty() ||
-        dependencesAreBlack_(currentNode, graphColor))
-    {
-      graphColor[currentNode.nodeId] = BLACK;
+  if (nodeIsEdge_(currentNode, graphColor)) {
+    auto state = std::find_if(
+        newGraph.begin(), newGraph.end(), [&currentNode](Node node) {
+          return (node.nodeId == currentNode.nodeId) ? true : false;
+        });
+    if (state == newGraph.end()) {
       newGraph.push_back(currentNode);
-      return;
     }
-
-    graphColor[currentNode.nodeId] = GRAY;
-    for (auto &dependenceIndex : currentNode.nodeDependences)
-    {
-      Node &dependenceNode = nodeById_(oldGraph, dependenceIndex);
-      deepSearchSorting_(graphColor, newGraph, oldGraph, dependenceNode);
-    }
-
-    graphColor[currentNode.nodeId] = BLACK;
-    newGraph.push_back(currentNode);
+    return;
   }
 
-} // namespace
+  graphColor[currentNode.nodeId] = GRAY;
+  for (auto &dependenceIndex : currentNode.nodeDependences) {
+    deepSearchEdges_(graphColor, newGraph, oldGraph,
+                     nodeById_(oldGraph, dependenceIndex));
+  }
+  graphColor[currentNode.nodeId] = WHITE;
+}
 
-void sortGraph(std::vector<Node> &graph)
-{
+}  // namespace
+
+void sortGraph(std::vector<Node> &graph) {
   auto inversedGraph = inverse_(graph);
-
-  std::vector<int> leafNodesId;
-  for (auto &node : inversedGraph)
-  {
-    if (node.nodeDependences.size() == 0)
-    {
-      leafNodesId.push_back(node.nodeId);
+  std::vector<Node> edgeNodes;
+  for (int i = 0; i < inversedGraph.size(); ++i) {
+    if (inversedGraph[i].nodeDependences.size() == 0) {
+      edgeNodes.push_back(graph[i]);
     }
   }
 
   std::vector<Node> newGraph;
-  std::map<int, int> graphColor;
-  newGraph.reserve(graph.size());
+  ColorDictionary graphColor;
 
-  for (auto &node : graph)
-  {
+  for (auto &node : graph) {
     graphColor.insert(NodeColor(node.nodeId, WHITE));
   }
 
-  for (auto &leaf : leafNodesId)
-  {
-    deepSearchSorting_(graphColor, newGraph, graph, nodeById_(graph, leaf));
+  while (not nodesAreBlack_(edgeNodes, graphColor)) {
+    for (auto &node : edgeNodes) {
+      deepSearchEdges_(graphColor, newGraph, graph, node);
+    }
+    for (auto &node : newGraph) {
+      graphColor[node.nodeId] = BLACK;
+    }
   }
 
   graph = newGraph;
 }
 
-int main()
-{
+int main() {
   Node node_0 = {0, std::vector<int>{}};
   Node node_1 = {1, std::vector<int>{0}};
   Node node_2 = {2, std::vector<int>{0, 1, 5}};
   Node node_3 = {3, std::vector<int>{1, 2}};
-  Node node_5 = {5, std::vector<int>{}};
-  Node node_4 = {4, std::vector<int>{3, 2}};
+  Node node_5 = {5, std::vector<int>{7}};
+  Node node_4 = {4, std::vector<int>{3, 2, 8}};
   Node node_6 = {6, std::vector<int>{2, 5, 3}};
+  Node node_7 = {7, std::vector<int>{}};
+  Node node_8 = {8, std::vector<int>{7}};
 
-  std::vector<Node> graph = {node_2, node_1, node_3, node_0,
-                             node_5, node_4, node_6};
+  // Node node_0 = {0, std::vector<int>{}};
+  // Node node_1 = {1, std::vector<int>{0}};
+  // Node node_2 = {2, std::vector<int>{1}};
+  // Node node_3 = {3, std::vector<int>{2}};
+  // Node node_5 = {5, std::vector<int>{1, 6}};
+  // Node node_4 = {4, std::vector<int>{1}};
+  // Node node_6 = {6, std::vector<int>{3, 1}};
+
+  std::vector<Node> graph = {node_2, node_1, node_3, node_0, node_5,
+                             node_4, node_6, node_7, node_8};
 
   sortGraph(graph);
 
